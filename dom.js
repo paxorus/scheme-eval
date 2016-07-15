07021c3132d0f90dbed494001cc65acd30214d34
paxorus <prakhar@brandeis.edu>
2016-07-15 01:34:14 -0400

    Primitive procedures
    Simple statements like (car (list 9 1 8)) and (+ 3 4) are supported.
    Next step is to support the other language constructs in eval().

--- dom.js
+++ dom.js
@@ -4,16 +4,17 @@
 */
 
 $(window).resize(function (evt) {
-	$("#editor").css("height",window.innerHeight-175);
-	$("#console").css("height",window.innerHeight-175);
+	$("#editor").css("height",window.innerHeight-125);
+	$("#console").css("height",window.innerHeight-125);
 	$("#editor").css("width",Math.floor(0.6*window.innerWidth));
 	$("#console").css("width",window.innerWidth-$("#editor").width());
+	$("#editor").linedtextarea();
 });
 
 $(document).ready(function () {
 	$(window).resize();
 	$("#run").on("click", function (evt) {
-		parse($("#editor").val());
-		// eval($("#textarea").val(),[]);
+		var value = interpret($("#editor").val());
+		$("#console").append(value+"<br>");
 	});
 });
--- eval.js
+++ eval.js
@@ -1,10 +1,256 @@
 /* Prakhar Sahay 07/13/2016
 
-
+procedure: ["procedure", params[], body, Env], change this to an object later
 */
 
-function tokenize(exp){// " (a (b c) d) " --> [a,(b c),d]
-	// exp = exp.trim();
+
+function eval(text, env){
+	var exp = parse(text);
+	// exp could be an array of strings or one string
+	if(isLiteral(exp)){
+		return evalLiteral(exp);
+	}else if(isAtom(exp)){
+		return lookupVariableValue(exp, env);
+	}else if(isApplication(exp)){
+		var proc = eval(exp[0], env);
+		var args = exp.slice(1).map(function(arg){
+			return eval(arg, env);
+		});
+		return apply(proc, args);
+	}else{
+		error("Unknown expression type -- EVAL", exp);
+	}
+}
+
+function apply(proc, args){
+	if(isPrimitiveProcedure(proc)){
+		return applyPrimitiveProcedure(proc, args);
+	}else if(isCompoundProcedure(proc)){
+		var newFrame = new Frame(proc[1], args);
+		var newEnv = proc[3].extend(newFrame);
+		return evalSequence(proc[2], newEnv);
+	}else{
+		error("Unknown procedure type -- APPLY", proc);
+	}
+}
+
+
+
+function evalSequence(exps, env){
+	var results = exps.map(function(exp){
+		return eval(exp);
+	});
+	return results[results.length-1];
+}
+
+function evalLiteral(exp){
+	if(isNumber(exp)){
+		return +exp;
+	}else if(isString(exp)){
+		return exp.slice(1,-1);
+	}else{
+		return (exp == "#t") ? true : false;
+	}
+}
+
+
+function isLiteral(exp){
+	return isAtom(exp) && (isNumber(exp) || isString(exp) || isBoolean(exp));
+}
+
+function isNumber(exp){
+	return !isNaN(+exp);// (NaN === NaN) is false
+}
+
+function isString(exp){
+	return exp[0] == "\"" && exp[exp.length-1] == "\"";
+}
+
+function isBoolean(exp){
+	return exp == "#t" || exp == "#f";
+}
+
+function isApplication(exp){
+	return exp instanceof Array;
+}
+
+function isAtom(exp){
+	return !(exp instanceof Array);
+}
+
+var primitiveProcedures = {
+	car: function(c){return c[0]},
+	cdr: function(c){return c[1]},
+	cons: function(x,y){assertNargs(arguments,2);return [x,y]},
+	list: function(){
+		if(arguments.length == 0){
+			return null;
+		}
+		var index = arguments.length - 1;
+		var cell = arguments[index];
+		for(;index>=0;index--){
+			cell = primitiveProcedures.cons(arguments[index], cell);
+		}
+		return cell;
+	},
+	map: function(proc, list){
+		assertNargs(arguments,2);
+		// assert procedure and list
+		if(list === null){
+			return null;
+		}else{
+			return primitiveProcedures.cons(
+				proc(primitiveProcedures.car(list)),
+				primitiveProcedures.map(proc, primitiveProcedures.cdr(list))
+			);
+		}
+	},
+	append: function(){
+		if(arguments.length == 0){
+			return null;
+		}
+		console.log("wip");
+	},
+	"null?": function(x){assertNargs(arguments,1);return x === null},
+	"zero?": function(x){assertNargs(arguments,1);assertNum(arguments);return x === 0},
+	"eq?": function(a,b){return a === b},// todo
+	"equal?": function(a,b){return a === b},// todo
+	"1+": function(a){assertNargs(arguments,1);assertNum(arguments);return a+1},
+	"-1+": function(a){assertNargs(arguments,1);assertNum(arguments);return a-1},
+	quotient: function(a,b){assertNargs(arguments,2);assertNum(arguments);return Math.floor(a/b)},
+	remainder: function(a,b){assertNargs(arguments,2);assertNum(arguments);return a%b},
+	"/": function(){
+		var args = fix(arguments);
+		assertNargs(args,">=1");
+		assertNum(args);
+		var dividend=args.unshift();
+		return args.reduce(function(a,b){return a/b},dividend);
+	},
+	"*": function(){
+		var args = fix(arguments);
+		assertNum(args);
+		return args.reduce(function(a,b){return a*b},1);
+	},
+	"+": function(){
+		var args = fix(arguments);
+		assertNum(args);
+		return args.reduce(function(a,b){return a+b},0);
+	},
+	"-": function(){
+		var args = fix(arguments);
+		assertNargs(args,">=1");
+		assertNum(args);
+		var minuend=args.unshift();
+		return args.reduce(function(a,b){return a-b},minuend);
+	},
+	"=": function(){
+		assertNargs(arguments,">=2");
+		assertNum(arguments);
+		for(var i=1;i<arguments.length;i++){
+			if(arguments[i-1] !== arguments[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+function isPrimitiveProcedure(proc){
+	return proc[0] == "primitive";
+}
+
+function isCompoundProcedure(proc){
+	return proc[0] == "procedure";
+}
+
+function applyPrimitiveProcedure(proc, args){
+	var implementation = proc[1];
+	return implementation.apply(undefined, args);
+}
+
+/**
+ * @param {object} baseEnv - Environment{} or null
+ * @param {object} newFrame - variable->value map
+ */
+
+function Environment(baseEnv, newFrame){
+	var _base = baseEnv;
+	var _lookup = newFrame;
+
+	this.get = function (varName) {
+		// if not this frame, search lower frames
+		if(_lookup[varName]){
+			return _lookup[varName];
+		}else if(_base === null){
+			// no lower frames
+			return undefined;
+		}else{
+			// recursively search lower
+			return _base.get(varName);
+		}
+	};
+
+	this.extend = function (newFrame) {
+		return new Environment(this,newFrame);
+	};
+}
+
+function Frame(varList, valList){
+	console.log("wip");
+	// error if list lengths differ
+	for(var i=0;i<varList.length;i++){
+		this[varList[i]] = valList[i];
+	}
+}
+
+function lookupVariableValue(varName, env){
+	var value = env.get(varName);
+	if(value === undefined){
+		error("Unbound variable", varName);
+	}
+	return value;
+}
+
+function setupEnvironment(){
+	var primitiveFrame = {};
+	Object.keys(primitiveProcedures).forEach(function (op) {
+		primitiveFrame[op] = ["primitive", primitiveProcedures[op]];
+	});
+	return new Environment(null, primitiveFrame);
+}
+
+
+// WARNINGS AND ERRORS
+
+function assertNargs(){
+	console.log("wip");
+}
+
+function assertNum(){
+	console.log("wip");
+}
+
+function error(message, obj){
+	// involve pretty-printing
+	console.log(message + "\n" + obj);
+}
+
+// SYNTAX PARSING
+
+function interpret(text){
+	text = text.split("\n").map(stripComment).join("\n");
+	text = text.trim();// an initial trim is necessary
+	var initialEnv = setupEnvironment();
+	return eval(text, initialEnv);
+}
+
+function parse(exp){
+	// todo: () matching
+	if(!exp.startsWith("(")){
+		return exp;
+	}
+
+	// " (a (b c) d) " --> [a,"(b c)",d]
 	var parts = [];
 
 	var buffer = "";
@@ -28,24 +274,7 @@ function tokenize(exp){// " (a (b c) d) " --> [a,(b c),d]
 			}
 		}
 	}
-	// console.log(parts);
 	return parts;
-}
-
-function traverse(exp,lvl){
-	tokenize(exp).forEach(function (token) {
-		if(token[0] == "(" && token[token.length-1] == ")"){
-			traverse(token, lvl+1);
-		}else{
-			console.log(token+" "+lvl);
-		}
-	});
-}
-
-function parse(text){
-	text = text.split("\n").map(stripComment).join("\n");
-	console.log(text);
-	traverse(text, 0);
 }
 
 function stripComment(line){
@@ -69,94 +298,6 @@ function isWhiteSpace(char){
 	return ["\n","\t"," "].indexOf(char) > -1;
 }
 
-function eval(exp, env){
-	if(isLiteral(exp)){
-		return exp;
-	}else if(isApplication(exp)){
-		apply();
-	}else{
-		error("Unknown expression type -- EVAL");
-	}
-}
-
-function apply(proc, args){
-
-}
-
-function isLiteral(exp){
-	return isNumber(exp) || isString(exp) || isBoolean(exp);
-}
-
-function isNumber(exp){
-	return +exp !== NaN;
-}
-
-function isString(exp){
-	return exp[0] == "\"" && exp[exp.length-1] == "\"";
-}
-
-function isBoolean(exp){
-	return exp == "#t" || exp == "#f";
-}
-
-function isPrimitiveProcedure(proc){
-	return proc[0] == "primitive";
-}
-
-function error(message){
-	console.log("uh oh");
-	console.log(message);
-}
-
-var primitiveProcedures = {
-	car: function(c){return c.x},
-	cdr: function(c){return c.y},
-	cons: function(x,y){assertNargs(arguments,2);return {x:x,y:y}},
-	"null?": function(x){assertNargs(arguments,1);return x === null},
-	"zero?": function(x){assertNargs(arguments,1);assertNum(arguments);return x === 0},
-	"eq?": function(a,b){return a === b},
-	"equal?": function(a,b){return a === b},
-	"1+": function(a){assertNargs(arguments,1);assertNum(arguments);return a+1},
-	"-1+": function(a){assertNargs(arguments,1);assertNum(arguments);return a-1},
-	quotient: function(a,b){assertNargs(arguments,2);assertNum(arguments);return Math.floor(a/b)},
-	remainder: function(a,b){assertNargs(arguments,2);assertNum(arguments);return a%b},
-	"/": function(){
-		assertNargs(arguments,">=1");
-		assertNum(arguments);
-		var dividend=arguments.unshift();
-		return arguments.reduce(function(a,b){return a/b},dividend);
-	},
-	"*": function(){
-		assertNum(arguments);
-		return arguments.reduce(function(a,b){return a*b},1);
-	},
-	"+": function(){
-		assertNum(arguments);
-		return arguments.reduce(function(a,b){return a+b},0);
-	},
-	"-": function(){
-		assertNargs(arguments,">=1");
-		assertNum(arguments);
-		var minuend=arguments.unshift();
-		return arguments.reduce(function(a,b){return a-b},minuend);
-	},
-	"=": function(){
-		assertNargs(arguments,">=2");
-		assertNum(arguments);
-		for(var i=1;i<arguments.length;i++){
-			if(arguments[i-1] !== arguments[i]){
-				return false;
-			}
-		}
-		return true;
-	}
-
-}
-
-function assertNargs(){
-	console.log("wip");
-}
-
-function assertNum(){
-	console.log("wip");
+function fix(arguments){
+	return Array.prototype.slice.call(arguments);
 }
