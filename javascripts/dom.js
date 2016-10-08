32565bb637bc1035eea332fcedd4cafe3e05619d
paxorus <prakhar@brandeis.edu>
2016-10-08 14:13:55 -0400

    Committing Ace
    Out-of-box Ace, need to add Scheme support.

---
+++ javascripts/dom.js
@@ -0,0 +1,19 @@
+/**
+ * Prakhar Sahay 07/13/2016
+ */
+
+$(window).resize(function (evt) {
+	$("#editor").css("height", window.innerHeight-125);
+	$("#console").css("height", window.innerHeight-125);
+	$("#editor").css("width",Math.floor(0.6*window.innerWidth));
+	$("#console").css("width",window.innerWidth-$("#editor").width());
+	// $("#editor").linedtextarea();
+});
+
+$(document).ready(function () {
+	$(window).resize();
+	$("#run").on("click", function (evt) {
+		var value = interpret(editor.getValue());
+		$("#console").append(value+"<br>");
+	});
+});
